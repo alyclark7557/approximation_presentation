ac643096e0826ff3a32d2ff32a7fd61c9fa321b2
leekmh <leekmh@dukes.jmu.edu>
2022-04-25 11:08:38 -0400

    Initial version

---
+++ knapsack_approximator.py
@@ -0,0 +1,12 @@
+def main():
+
+
+#input format
+    # W weight we can take from the bag
+    #n items in the bag
+    #n lines of the format "ITEM Value Weight"
+
+
+  print("lets approximate max knapsack!")
+if __name__ == "__main__":
+    main()
