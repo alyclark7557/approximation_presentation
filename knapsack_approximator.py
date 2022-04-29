2c371f0a2f28c3332ad3c9f5f050b9f49fe373b6
leekmh <leekmh@dukes.jmu.edu>
2022-04-29 10:46:01 -0400

    approximator should be working

--- knapsack_approximator.py
+++ knapsack_approximator.py
@@ -3,11 +3,20 @@ import sys
 import time
 def Knapsack_approximation(sack, weight):
  print("just show that I am running") 
-# i probably actually dont need a recursive helper
- 
-#  def recursive_helper(weight_left,index):
-     
-
+ # sort the sack
+ # add items to the list until i am out of available weight
+ sack.sort(key=lambda x: x[1] / x[2], reverse=True) 
+ print(sack)   
+ ret_value = []
+ keep_going = True
+ index = 0
+ weight_remaining = weight
+ while (weight_remaining > 0 and index < len(sack)):
+     if sack[index][2] <= weight_remaining:
+         ret_value.append(sack[index])
+         index = index + 1
+         weight_remaining -= sack[index][2]
+ return ret_value
 def main():
 
 
@@ -17,7 +26,7 @@ def main():
     #n lines of the format "ITEM Value Weight"
 
 
-  print("lets approximate max knapsack!")
+#  print("lets approximate max knapsack!")
   # code for taking in input , use knapsack_input.py to generate a file of the appropriate format
   w = float(input())
   n = int(input())
@@ -27,21 +36,23 @@ def main():
     i[1] = float(i[1])
     i[2] = float(i[2])
     items.append(i)
-  print(items)
+#  print(items)
 
 
 #start knapsack code
   start_time = time.time()
 #  t_val, items_n_sack = Knapsack_approximation(items,w)
-  Knapsack_approximation(items,w)
-#  end = time.time()
-#  print("Results:")
-#  print("    Total Value: %0.2f" % t_val)
-#  print("    Items:")
-#  for i in items_n_sack:
-#     print("        ", i[0], i[1], i[2])
-#  print("")
-#  print("Time taken: %fs" % (end- start_time))
-  print("we got to the end!")
+  items_chosen = Knapsack_approximation(items,w)
+  end = time.time()
+  total_value = 0
+  print(items_chosen)
+  print("Results:")
+  print("    Items:")
+  for i in items_chosen:
+     total_value += i[1]
+     print("        ", i[0], i[1], i[2])
+  print("")
+  print("Time taken: %fs" % (end- start_time))
+  print("    Total Value: %0.2f" % total_value)
 if __name__ == "__main__":
     main()
